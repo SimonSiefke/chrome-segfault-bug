d41f6dc5e5609f14113eadd9503327221454cc1e
Simon Siefke <simon.siefke@gmail.com>
2021-02-11 22:52:05 +0100

    update reproduction steps

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // To reproduce the issue:
 // 1. Open the page in Chrome
 // 2. in chrome devtools select "break on any xhr"
-// 3. Once it is paused, comment out the code in this file
-// 4. Reload the page
+// 3. Once it is paused, reload the page
+// 4. Chrome tab crashes with Segfault error
 
 setInterval(async () => {
   const response = await fetch('https://www.google.com', { mode: 'no-cors' })
